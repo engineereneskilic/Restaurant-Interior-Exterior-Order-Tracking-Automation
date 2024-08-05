94180fb8ab7c1fab059221ce3e3081376375f100
Enes KILIÇ <engineereneskilic@gmail.com>
2024-08-05 20:17:00 +0300

    Add files via upload

---
+++ lokanta_siparis_takip_programi/giris.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace lokanta_siparis_takip_programi
+{
+    partial class giris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // giris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "giris";
+            this.Text = "giris";
+            this.Load += new System.EventHandler(this.giris_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
